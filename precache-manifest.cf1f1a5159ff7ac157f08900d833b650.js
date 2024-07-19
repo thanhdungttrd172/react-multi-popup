86f6a9886716e22a888fc7e1c9ff8b4c15ff5f67
dungnt <thanhdungttrd@gmail.com>
2024-07-19 17:25:21 +0700

    Updates

---
+++ precache-manifest.cf1f1a5159ff7ac157f08900d833b650.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b41199b453b80565acfef68690f9cc9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d3153dbce4e3a515ddac",
+    "url": "./static/css/main.cca6cab1.chunk.css"
+  },
+  {
+    "revision": "55ad97b0b574d8243dd5",
+    "url": "./static/js/2.e7e896a5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.e7e896a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3153dbce4e3a515ddac",
+    "url": "./static/js/main.a2c2aa94.chunk.js"
+  },
+  {
+    "revision": "0d267eb49d8f63893045",
+    "url": "./static/js/runtime-main.ca940919.js"
+  }
+]);
